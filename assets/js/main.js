b445d2a810b26af3f43dd4452ef180473f0506c1
gluke0 <milanesigianluca@gmail.com>
2023-04-20 15:36:57 +0200

    aggiunta tasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,45 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+
+            newT: '',
+
+            todos: [
+                {
+                    text: 'Chiamare la banca',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                },
+                {
+                    text: 'Comprare le uova',
+                    done: false
+                },
+                {
+                    text: 'Fare i compiti',
+                    done: true
+                }
+            ]
         }
     },
+
     methods:{
+        newToDo(){
+            if(this.newT.split() != ''){
+                const task = {
+                    text: this.newT,
+                    done: false,
+                }
+                this.todos.unshift(task);
+                this.newT = '';
+            }
+        },
         
     }
 }).mount('#app')
